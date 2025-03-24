2c74633ee0040092f064925f66db2804f3e15837
ANASWARA <anaswarajohnson463@gmail.com>
2025-03-24 12:31:25 +0000

    module list and filter

--- app/src/main/java/ul/ed5042_lab2/ui/QcaScreen.kt
+++ app/src/main/java/ul/ed5042_lab2/ui/QcaScreen.kt
@@ -29,7 +29,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
+
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -198,9 +198,7 @@ fun ModuleList(moduleList: List<Module>, modifier: Modifier) {
             )
         }
     }
-    moduleList.forEach{
-        module->
-    }
+
 }
 
 
